fe09439b0b4737e264dc28ca0de419a9923e5a4d
Demon <gsc@demongao.com>
2025-04-06 11:49:27 +0800

    初始化 hardhat 项目

---
+++ L0/02.Solidity经典用例/solidity-code/hardhat.config.ts
@@ -0,0 +1,19 @@
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+import dotenv from 'dotenv'
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PK!]
+    }
+  }
+};
+
+
+
+export default config;
